e518651f7c59d9939c3eeb7174d030e7d741c43e
b0m313 <zxx0313@inu.ac.kr>
2023-04-24 00:06:02 +0900

    Refactor: flask_app_test.py

--- flask_app_test.py
+++ flask_app_test.py
@@ -1,62 +1,57 @@
 import threading
 import time
-from flask import Flask, jsonify, request
-from werkzeug.utils import secure_filename
 import os
 import shutil
-from PIL import Image
-from os import path
 import json
-import requests
-import revise
-
 import subprocess
-
+import requests
+from flask import Flask, jsonify, request
+from PIL import Image
+from werkzeug.utils import secure_filename
+from os import path
+import revise
 from revise import server
 
-# Flask app
-app = Flask(__name__)  # Flask 클래스를 객체화시켜서 app이라는 변수에 저장
+app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-# create 함수를 end-point로 등록
-@app.route("/image/userid", methods=['POST'])
-def img():
-    # form-data로 전달된 파일을 받기 위해 request.files 사용
-    # 'file'은 클라이언트 측에서 파일을 전송할 때 사용한 필드명
-    img = request.files.get('file')
-    params = request.form
-
-    # 해당 datasets을 넣을 폴더 생성
-    path_img = './dataset/additional-training-datasets/' + params['userid']
-
-    # 이미 폴더가 있는 경우 해당 폴더 삭제
+def create_folder(path_img):
     if path.exists(path_img):
         shutil.rmtree(path_img)
-
-    # 폴더 생성
     os.mkdir(path_img)
 
-    # 이미지 저장
-    for i, image in enumerate(request.files.getlist('file')):
+def save_images(images, path_img):
+    for i, image in enumerate(images):
         save_img = Image.open(image)
-        save_img.save(path_img + '/' + str(i) + '.jpeg', format='JPEG')
+        save_img.save(f"{path_img}/{i}.jpeg", format="JPEG")
 
-    output1 = subprocess.run(["find", ".", "-name", "\"*.DS_Store\"", "-type", "f", "-delete"], capture_output=True)
+def run_commands():
+    output1 = subprocess.run(
+        ["find", ".", "-name", "\"*.DS_Store\"", "-type", "f", "-delete"],
+        capture_output=True,
+    )
     print(output1.stdout.decode())
 
-    output2 = subprocess.run(["python3", "train.py", "--is-add-user=True"], capture_output=True)
+    output2 = subprocess.run(
+        ["python3", "train.py", "--is-add-user=True"], capture_output=True
+    )
     print(output2.stdout.decode())
 
-    return 'ok'
+@app.route("/image/userid", methods=["POST"])
+def img():
+    img = request.files.get("file")
+    params = request.form
+    path_img = f"./dataset/additional-training-datasets/{params['userid']}"
+    create_folder(path_img)
+    save_images(request.files.getlist("file"), path_img)
+    run_commands()
+
+    return "ok"
 
 def send_result_to_external_api(user_id):
-    url = ''        # add URL
-    data = {
-        "userId": user_id
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    url = "http://strongsumin.milk717.com/api/push/userId"
+    data = {"userId": user_id}
+    headers = {"Content-Type": "application/json"}
     try:
         response = requests.post(url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
@@ -66,22 +61,22 @@ def send_result_to_external_api(user_id):
     except Exception as err:
         print(f"Error occurred: {err}")
 
-@app.route('/image/result', methods=['POST'])
+@app.route("/image/result", methods=["POST"])
 def result():
     data = request.get_json()
-    print("userId: {}".format(data['name']))
-    send_result_to_external_api(data['name'])
-    return "ok"
+    print(f"userId: {data['name']}")
+    send_result_to_external_api(data["name"])
 
+    return data["name"]
 
-if __name__ == '__main__':
-    def run_flask_app():
-        app.run(host='0.0.0.0', port=13330, debug=True, use_reloader=False)
+def run_flask_app():
+    app.run(host="0.0.0.0", port=13330, debug=True, use_reloader=False)
 
+if __name__ == "__main__":
     flask_thread = threading.Thread(target=run_flask_app)
     flask_thread.start()
 
-    time.sleep(3)  # Give the Flask app time to start
+    time.sleep(3)
 
     server_thread = threading.Thread(target=revise.server)
     server_thread.start()
